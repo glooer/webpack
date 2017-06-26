e8e8b9e3387d9dc9982476d956dc3ece94b9e870
Kostya Polenkov <kostya14@gmail.com>
2017-06-27 00:10:58 +0500

    подклчюен бутстрап (без sass)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import _ from 'lodash';
+// import 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import './style.css';
 
 function component() {
   var element = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,37 @@
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    // rules: [
+    //   {
+    //     test: /\.css$/,
+    //     use: [
+    //       'style-loader',
+    //       'css-loader'
+    //     ]
+    //   }
+    // ],
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('bundle.css'),
+    new webpack.ProvidePlugin({ // inject ES5 modules as global vars
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      Tether: 'tether'
+    })
+  ]
 };
